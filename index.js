40b3b0cf1da59fa2772438e5b0833e2e052aa8e7
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-07 22:49:36 +0300

    This.This one is tough

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+ 
+    function createEmployeeRecord ([firstName,familyName, title, payPerHour]) {
+        let employeeDetails = {
+            firstName: firstName,
+            familyName: familyName,
+            title:title,
+            payPerHour:  payPerHour,
+            timeInEvents: [],
+            timeOutEvents: []
+
+        }
+return employeeDetails
+    }
+
+
+
+    function createEmployeeRecords () {
+        let arrayofObjects = [{
+
+        }]
+
+
+    }
+
+
+    
